16864f552de88ae29919e01b64b7af51f6832739
Dennis, Kim <bluedskim@gmail.com>
2022-12-21 10:49:05 +0900

    change static generator to get args

--- lib/src/main/java/net/shed/mlrbinder/verb/Verb.java
+++ lib/src/main/java/net/shed/mlrbinder/verb/Verb.java
@@ -11,69 +11,69 @@ public class Verb implements Arg {
 	/**
 	 * verbs can be chained with CHAINING_ADVERB
 	 */
-	public static final Verb altkv() {return new Verb("altkv");}
-	public static final Verb bar() {return new Verb("bar");}
-	public static final Verb bootstrap() {return new Verb("bootstrap");}
-	public static final Verb cat() {return new Verb("cat");}
-	public static final Verb check() {return new Verb("check");}
-	public static final Verb cleanWhitespace() {return new Verb("clean-whitespace");}
-	public static final Verb count() {return new Verb("count");}
-	public static final Verb countDistinct() {return new Verb("count-distinct");}
-	public static final Verb countSimilar() {return new Verb("count-similar");}
-	public static final Verb cut() {return new Verb("cut");}
-	public static final Verb decimate() {return new Verb("decimate");}
-	public static final Verb fillDown() {return new Verb("fill-down");}
-	public static final Verb fillEmpty() {return new Verb("fill-empty");}
-	public static final Verb filter() {return new Verb("filter");}
-	public static final Verb flatten() {return new Verb("flatten");}
-	public static final Verb formatValues() {return new Verb("format-values");}
-	public static final Verb fraction() {return new Verb("fraction");}
-	public static final Verb gap() {return new Verb("gap");}
-	public static final Verb grep() {return new Verb("grep");}
-	public static final Verb groupBy() {return new Verb("group-by");}
-	public static final Verb groupLike() {return new Verb("group-like");}
-	public static final Verb havingFields() {return new Verb("having-fields");}
-	public static final Verb head() {return new Verb("head");}
-	public static final Verb histogram() {return new Verb("histogram");}
-	public static final Verb join() {return new Verb("join");}
-	public static final Verb jsonParse() {return new Verb("json-parse");}
-	public static final Verb jsonStringify() {return new Verb("json-stringify");}
-	public static final Verb label() {return new Verb("label");}
-	public static final Verb latin1ToUtf8() {return new Verb("latin1-to-utf8");}
-	public static final Verb utf8ToLatin1() {return new Verb("utf8-to-latin1");}
-	public static final Verb leastFrequent() {return new Verb("least-frequent");}
-	public static final Verb mergeFields() {return new Verb("merge-fields");}
-	public static final Verb mostFrequent() {return new Verb("most-frequent");}
-	public static final Verb nest() {return new Verb("nest");}
-	public static final Verb nothing() {return new Verb("nothing");}
-	public static final Verb put() {return new Verb("put");}
-	public static final Verb regularize() {return new Verb("regularize");}
-	public static final Verb removeEmptyColumns() {return new Verb("remove-empty-columns");}
-	public static final Verb rename() {return new Verb("rename");}
-	public static final Verb reorder() {return new Verb("reorder");}
-	public static final Verb repeat() {return new Verb("repeat");}
-	public static final Verb reshape() {return new Verb("reshape");}
-	public static final Verb sample() {return new Verb("sample");}
-	public static final Verb sec2Gmt() {return new Verb("sec2gmt");}
-	public static final Verb sec2Gmtdate() {return new Verb("sec2gmtdate");}
-	public static final Verb seqgen() {return new Verb("seqgen");}
-	public static final Verb shuffle() {return new Verb("shuffle");}
-	public static final Verb skipTrivialRecords() {return new Verb("skip-trivial-records");}
+	public static final Verb altkv(Arg... args) {return new Verb("altkv", args);}
+	public static final Verb bar(Arg... args) {return new Verb("bar", args);}
+	public static final Verb bootstrap(Arg... args) {return new Verb("bootstrap", args);}
+	public static final Verb cat(Arg... args) {return new Verb("cat", args);}
+	public static final Verb check(Arg... args) {return new Verb("check", args);}
+	public static final Verb cleanWhitespace(Arg... args) {return new Verb("clean-whitespace", args);}
+	public static final Verb count(Arg... args) {return new Verb("count", args);}
+	public static final Verb countDistinct(Arg... args) {return new Verb("count-distinct", args);}
+	public static final Verb countSimilar(Arg... args) {return new Verb("count-similar", args);}
+	public static final Verb cut(Arg... args) {return new Verb("cut", args);}
+	public static final Verb decimate(Arg... args) {return new Verb("decimate", args);}
+	public static final Verb fillDown(Arg... args) {return new Verb("fill-down", args);}
+	public static final Verb fillEmpty(Arg... args) {return new Verb("fill-empty", args);}
+	public static final Verb filter(Arg... args) {return new Verb("filter", args);}
+	public static final Verb flatten(Arg... args) {return new Verb("flatten", args);}
+	public static final Verb formatValues(Arg... args) {return new Verb("format-values", args);}
+	public static final Verb fraction(Arg... args) {return new Verb("fraction", args);}
+	public static final Verb gap(Arg... args) {return new Verb("gap", args);}
+	public static final Verb grep(Arg... args) {return new Verb("grep", args);}
+	public static final Verb groupBy(Arg... args) {return new Verb("group-by", args);}
+	public static final Verb groupLike(Arg... args) {return new Verb("group-like", args);}
+	public static final Verb havingFields(Arg... args) {return new Verb("having-fields", args);}
+	public static final Verb head(Arg... args) {return new Verb("head", args);}
+	public static final Verb histogram(Arg... args) {return new Verb("histogram", args);}
+	public static final Verb join(Arg... args) {return new Verb("join", args);}
+	public static final Verb jsonParse(Arg... args) {return new Verb("json-parse", args);}
+	public static final Verb jsonStringify(Arg... args) {return new Verb("json-stringify", args);}
+	public static final Verb label(Arg... args) {return new Verb("label", args);}
+	public static final Verb latin1ToUtf8(Arg... args) {return new Verb("latin1-to-utf8", args);}
+	public static final Verb utf8ToLatin1(Arg... args) {return new Verb("utf8-to-latin1", args);}
+	public static final Verb leastFrequent(Arg... args) {return new Verb("least-frequent", args);}
+	public static final Verb mergeFields(Arg... args) {return new Verb("merge-fields", args);}
+	public static final Verb mostFrequent(Arg... args) {return new Verb("most-frequent", args);}
+	public static final Verb nest(Arg... args) {return new Verb("nest", args);}
+	public static final Verb nothing(Arg... args) {return new Verb("nothing", args);}
+	public static final Verb put(Arg... args) {return new Verb("put", args);}
+	public static final Verb regularize(Arg... args) {return new Verb("regularize", args);}
+	public static final Verb removeEmptyColumns(Arg... args) {return new Verb("remove-empty-columns", args);}
+	public static final Verb rename(Arg... args) {return new Verb("rename", args);}
+	public static final Verb reorder(Arg... args) {return new Verb("reorder", args);}
+	public static final Verb repeat(Arg... args) {return new Verb("repeat", args);}
+	public static final Verb reshape(Arg... args) {return new Verb("reshape", args);}
+	public static final Verb sample(Arg... args) {return new Verb("sample", args);}
+	public static final Verb sec2Gmt(Arg... args) {return new Verb("sec2gmt", args);}
+	public static final Verb sec2Gmtdate(Arg... args) {return new Verb("sec2gmtdate", args);}
+	public static final Verb seqgen(Arg... args) {return new Verb("seqgen", args);}
+	public static final Verb shuffle(Arg... args) {return new Verb("shuffle", args);}
+	public static final Verb skipTrivialRecords(Arg... args) {return new Verb("skip-trivial-records", args);}
 	public static final Verb sort(Arg... args) {return new Verb("sort", args);}
-	public static final Verb sortWithinRecords() {return new Verb("sort-within-records");}
-	public static final Verb split() {return new Verb("split");}
-	public static final Verb stats1() {return new Verb("stats1");}
-	public static final Verb stats2() {return new Verb("stats2");}
-	public static final Verb step() {return new Verb("step");}
-	public static final Verb summary() {return new Verb("summary");}
-	public static final Verb tac() {return new Verb("tac");}
-	public static final Verb tail() {return new Verb("tail");}
-	public static final Verb tee() {return new Verb("tee");}
-	public static final Verb template() {return new Verb("template");}
-	public static final Verb top() {return new Verb("top");}
-	public static final Verb unflatten() {return new Verb("unflatten");}
-	public static final Verb uniq() {return new Verb("uniq");}
-	public static final Verb unsparsify() {return new Verb("unsparsify");}
+	public static final Verb sortWithinRecords(Arg... args) {return new Verb("sort-within-records", args);}
+	public static final Verb split(Arg... args) {return new Verb("split", args);}
+	public static final Verb stats1(Arg... args) {return new Verb("stats1", args);}
+	public static final Verb stats2(Arg... args) {return new Verb("stats2", args);}
+	public static final Verb step(Arg... args) {return new Verb("step", args);}
+	public static final Verb summary(Arg... args) {return new Verb("summary", args);}
+	public static final Verb tac(Arg... args) {return new Verb("tac", args);}
+	public static final Verb tail(Arg... args) {return new Verb("tail", args);}
+	public static final Verb tee(Arg... args) {return new Verb("tee", args);}
+	public static final Verb template(Arg... args) {return new Verb("template", args);}
+	public static final Verb top(Arg... args) {return new Verb("top", args);}
+	public static final Verb unflatten(Arg... args) {return new Verb("unflatten", args);}
+	public static final Verb uniq(Arg... args) {return new Verb("uniq", args);}
+	public static final Verb unsparsify(Arg... args) {return new Verb("unsparsify", args);}
 	
 	String verbName;
 
